ab4eca64d4532538867fe5a48d4afec70f38d8b9
SenecaX <kedaren.dorasami@gmail.com>
2025-11-13 18:46:53 +0400

    ref

--- demo/run-uc01-integration.js
+++ demo/run-uc01-integration.js
@@ -3,8 +3,9 @@ import { RuntimeEngine } from "../engine/runtime-engine.js";
 const runtime = new RuntimeEngine();
 
 runtime.init();
-runtime.run("let x = 2;");
+runtime.run("var x = 2; let y = 3; const z = 6;");
 console.log("x =", runtime.resolve("x"));
+console.log("x =", runtime.resolve("y"));
 
 runtime.terminate();
 
